e3976ca4628498dfd282f5844bed517aa33806a2
NataliiaHolovash <n.golovash@ukr.net>
2024-06-25 23:29:17 +0200

    add app code

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+// src/App.jsx
+
+
+import Product from './Product';
+import Mailbox from './Mailbox';
+import Alert from './Alert';
+
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+
+     
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+
+      <Mailbox
+        name="Polina"
+        unreadMessages="coucou"
+      />
+      
+    </div>
+  );
+}
